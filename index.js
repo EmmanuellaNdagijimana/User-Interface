3026de91b05a61556512f9b1150ce45f68dd89b6
Emmanueli <Ndagijimanaemmanueli@akilahinstitute.org>
2019-10-15 10:02:04 +0200

    challenge 3 tests

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './User-Interface/server/routes/route'; 
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', router);
+
+const PORT = process.env.PORT || 4002;
+app.listen( PORT, function(){
+ console.log(`server listern on PORT ${PORT}`)
+});
+
+export default app;
--- server/middlewares/validator.js
+++
@@ -1,7 +0,0 @@
-const validateEntry = (req, res, next) => {
-    const {title} = req.body;
-    if (!title.trim()) {return res.send('error')}
-    next();
-}
-
-export {validateEntry};
